7683acf6c05a46e93b76289e84122d2768813c13
Anthony15224 <aces15224@hotmail.com>
2023-01-06 14:37:24 -0600

    JawsDB and connection updates

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,47 @@
 const { Sequelize } = require('sequelize');
+// var mysql = require('mysql');
 
-var sequelize = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });  
+  // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  //   if (err) throw err;
+  
+  //   console.log('The solution is: ', rows[0].solution);
+  // });
+  
+  connection.end();
+} else{
+  var sequelize = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
     host: "localhost",
     port: 3306,
     dialect: "mysql",
     pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
+      max: 5,
+      min: 0,
+      idle: 10000
     }
-});
+  });
 
-try {
+  try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-// try {
-//     console.log("Connections")
-//     sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-// } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
+
+
+}
+
+
+
   
 module.exports = sequelize;
