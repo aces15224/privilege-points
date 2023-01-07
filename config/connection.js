2a3a32c93aeefd7330bb9c4c378817879795338f
Anthony15224 <aces15224@hotmail.com>
2023-01-07 12:29:17 -0600

    .

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize');
-var connection;
+var mysql = require('mysql');
+
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  
-} else{
-  connection = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
+} else {
+  var connection  = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
     host: "localhost",
     port: 3306,
     dialect: "mysql",
@@ -14,27 +15,52 @@ if (process.env.JAWSDB_URL) {
       idle: 10000
     }
   });
-
-  try {
-    await connection.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-
-
 }
 
-connection.connect(function (err) {
+// Make connection.
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
-});  
+});
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
+// } else{
+//   connection = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
+//     host: "localhost",
+//     port: 3306,
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       idle: 10000
+//     }
+//   });
+
+//   try {
+//     await connection.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
 
 
-connection.end();
+// }
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });  
+
+
+// connection.end();
 
   
 module.exports = connection;
