422342b834f683d64108fb09baa37df7b3788248
Anthony15224 <aces15224@hotmail.com>
2023-01-08 07:01:51 -0600

    L

--- config/connection.js
+++ config/connection.js
@@ -2,35 +2,10 @@ const { Sequelize } = require('sequelize');
 // var mysql = require('mysql');
 
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection  = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  });
-}
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 // if (process.env.JAWSDB_URL) {
 //   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  
-// } else{
-//   connection = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
+// } else {
+//   var connection  = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
 //     host: "localhost",
 //     port: 3306,
 //     dialect: "mysql",
@@ -40,27 +15,52 @@ connection.connect(function(err) {
 //       idle: 10000
 //     }
 //   });
-
-//   try {
-//     await connection.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-
-
 // }
 
-// connection.connect(function (err) {
+// // Make connection.
+// connection.connect(function(err) {
 //   if (err) {
 //     console.error("error connecting: " + err.stack);
 //     return;
 //   }
 //   console.log("connected as id " + connection.threadId);
-// });  
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
+} else{
+  var sequelize = new Sequelize("privilegePoints_db", "root", "2Thbrush!", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+
+  try {
+    await connection.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 
-// connection.end();
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});  
+
+
+connection.end();
 
   
-module.exports = connection;
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const db = require('./models');
 //evironmental variables
 require('dotenv').config()
