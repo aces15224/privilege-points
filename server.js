2ca01afe43e37f9ec70efd1e7f95d20f0c459943
Anthony15224 <aces15224@hotmail.com>
2023-01-08 07:41:16 -0600

    react-scripts edit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const db = require('./models');
@@ -31,6 +31,11 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(apiRoutes);
+
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === 'production') {
