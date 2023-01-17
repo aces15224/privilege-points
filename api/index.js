e1b0348236234b8cc94d6425e95da78e6218fbb7
Anthony15224 <aces15224@hotmail.com>
2023-01-17 14:28:08 -0600

    v

--- api/index.js
+++ api/index.js
@@ -33,9 +33,9 @@ app.use(express.json());
 app.use(apiRoutes);
 
 // app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === 'production') {
